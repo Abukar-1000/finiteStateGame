cf35b7a2e1e39c82d8a0bdc93ded22a83cbaf8b5
Abukar-1000 <abukardjama1@gmail.com>
2023-05-11 21:23:12 -0400

    + header

---
+++ src/MyHeader.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+
+/*
+    creating a custom header responsible for animatin the text MAFIA on screen
+*/
+function MyHeader() {
+
+    // animation
+    useEffect((event) => {
+        // start the animation when the page is ready, will insert a character every 20 ms js delay
+        let mafiaText = "MAFIA";
+        let spanText = "";
+        const outputSpan = document.querySelector(".mafiaHeaderTxt");
+        // grab each charater in "MAFIA" and store in an array
+        const arr = mafiaText.split("");
+        let intervalID = setInterval(grabNext,300);
+        let index = 0;
+
+        // function to animate each char of "MAFIA"
+        function grabNext() {
+
+            // stop writing words to output
+            if (index >= arr.length) {
+                clearInterval(intervalID);
+                intervalID = null;
+                return;
+            }
+
+            // update the state
+            outputSpan.textContent += arr[index];
+            // spanText += arr[index];
+            // updateSpanState(spanText);
+            index += 1;
+            console.log(index);
+        }
+    });
+    // animation state is held in the spanText variable
+    return (
+        <header>
+            <header className="">
+                <h1>Let's Play <span className='mafiaHeaderTxt'></span></h1>
+            </header>
+        </header>
+    );
+}
+
+
+export default MyHeader;
